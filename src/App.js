7004fdaee18301a866c84e07e7fc8f433c94012a
Lennie Nurse <lnursej@gmail.com>
2023-02-17 09:28:23 -0500

    "updated still not seeing nothing"

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import RouteComponent from "./Components/RouteComponent"
 import "./App.css"
+import NavBar from "./Components/NavBar"
+// import Footer from "./Components/Footer"
 
 function App() {
   return (
     <div className="container">
+      <NavBar />
       <RouteComponent />
+      {/* <Footer /> */}
     </div>
   )
 }
--- src/Components/Provider.js
+++ src/Components/Provider.js
@@ -1,47 +1,47 @@
-// import { createContext, useContext, useState } from "react"
-// import NavBar from "../Components/NavBar"
-// import Footer from "./Footer"
-// import { useNavigate } from "react-router-dom"
-// import axios from "axios"
+import { createContext, useContext, useState } from "react"
+import NavBar from "../Components/NavBar"
+import Footer from "./Footer"
+import { useNavigate } from "react-router-dom"
+import axios from "axios"
 
-// //  this creates the use custom hook object which can be imported with useContext with data inside of it
-// export const ArchiveContext = createContext()
+//  this creates the use custom hook object which can be imported with useContext with data inside of it
+export const ArchiveContext = createContext()
 
-// export function useArchiveContext() {
-//   return useContext(ArchiveContext)
-// }
+export function useArchiveContext() {
+  return useContext(ArchiveContext)
+}
 
-// // this
-// export const ArchiveProvider = ({ Children }) => {
+// this
+export const ArchiveProvider = ({ Children }) => {
+  const navigate = useNavigate()sdfa
+  const API = process.env.REACT_APP_API_URL
+  const [games, setGames] = useState({
+    title: " ",
+    platform: " ",
+    genre: " ",
+    number_of_players: " ",
+    ESRD_rating: " ",
+    publisher: " ",
+    developer: " ",
+    release_date: " ",
+    present: false,
+    digital: false,
+    image: " ",
+    description: " ",
+  })
 
-//   const API = process.env.REACT_APP_API_URL
-//   const [games, setGames] = useState({
-//     title: " ",
-//     platform: " ",
-//     genre: " ",
-//     number_of_players: " ",
-//     ESRD_rating: " ",
-//     publisher: " ",
-//     developer: " ",
-//     release_date: " ",
-//     present: false,
-//     digital: false,
-//     image: " ",
-//     description: " ",
-//   })
-
-//   return (
-//     <ArchiveContext.Provider
-//       value={{
-//         API,
-//         axios,
-//         games,
-//         setGames,
-//       }}
-//     >
-//       <NavBar />
-//       {Children}
-//       <Footer />
-//     </ArchiveContext.Provider>
-//   )
-// }
+  return (
+    <ArchiveContext.Provider
+      value={{
+        API,
+        axios,
+        games,
+        setGames,
+      }}
+    >
+     
+      {Children}
+      
+    </ArchiveContext.Provider>
+  )
+}
